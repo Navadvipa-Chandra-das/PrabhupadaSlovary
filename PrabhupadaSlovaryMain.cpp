55311fd5b9cdd797be921def6ef27c4f228f3000
Navadvipa Chandra das <navadvipa.chandra.das@nizhnyaya-navadvipa.ru>
2020-02-04 22:04:12 +0300

    Revert "V4.1"
    This reverts commit ef8890f6ea4aad305297c32eb936aa44172ae3f1.

--- PrabhupadaSlovaryMain.cpp
+++ PrabhupadaSlovaryMain.cpp
@@ -5,6 +5,7 @@
 GUI_APP_MAIN
 {
   Upp::StdLogSetup( Upp::LOG_FILE | Upp::LOG_COUT | Upp::LOG_TIMESTAMP );
+
   const Upp::Vector< Upp::String >& cl = Upp::CommandLine();
 
   Prabhupada::CommandMap cm;
